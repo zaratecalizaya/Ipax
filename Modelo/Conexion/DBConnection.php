1b63c83fa7d520f033b7d5b2c870129c7b482461
cartory <xXcartoryXx@gmail.com>
2021-08-21 09:26:57 -0400

    DBConnection por las tardas

--- Modelo/Conexion/DBConnection.php
+++ Modelo/Conexion/DBConnection.php
@@ -8,7 +8,17 @@ class DBConnection
 	public static function query($sql)
 	{
 		$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+		if (!$link) {
+			die('⚠️ Connection Error ⚠️' . mysqli_connect_error());
+		}
+
 		$result = mysqli_query($link, $sql);
+
+		if (!$result) {
+			die('⚠️ No results Available ⚠️' . mysqli_error($link));
+		}
+
 		mysqli_close($link);
 
 		return $result;
